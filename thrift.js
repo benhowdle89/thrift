334ca3df7550cedd874decae0fda0ebac7ddefe5
Ben Howdle <benhowdle89@gmail.com>
2013-04-29 20:10:10 +0100

    tetsing

--- thrift.js
+++ thrift.js
@@ -211,8 +211,29 @@ if (Meteor.isClient) {
   });
 
 
+
 }
 
 if (Meteor.isServer) {
+  Meteor.startup(function() {
+    // code to run on server at startup
 
+  });
+
+  Meteor.publish("Items", function() {
+    return Items.find();
+  });
+
+  Items.allow({
+    'insert': function(userId, doc) {
+      if (userId) {
+        return true;
+      }
+    },
+    'remove': function(userId, doc) {
+      if (userId == doc.userId) {
+        return true;
+      }
+    }
+  });
 }
